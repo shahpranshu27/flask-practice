b971502564e634abdac257ba578ffdf1aaac6d7c
shahpranshu27 <shahpranshu2712@gmail.com>
2026-02-10 22:21:45 +0530

    flask basic application

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Static route
 @app.route("/")
-def hello():
+def hello(): # functions like these are called view functions
     return {"message": "Hello world!"}
+    # return '<h1>Hello World!</h1>'
+    
+# Dynamic route
+@app.route('/user/<name>')
+def user(name):
+    return f"User {name}"
+    
+@app.route('/user/<int:id>')
+def id(id):
+    return f"Number {id}"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=7979)
+    app.run(debug=True)
